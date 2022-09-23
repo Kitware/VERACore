1e169fcb2d023ad4aee80cfb310f179aa7b335b8
Patrick Avery <patrick.avery@kitware.com>
2022-09-23 12:10:13 -0400

    fix(core_view): clean up core view generation code
    We were doing a lot of stuff that we don't need to anymore. Clean it up.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- vera_core/app/ui/core_view.py
+++ vera_core/app/ui/core_view.py
@@ -16,76 +16,36 @@ def initialize(server, vera_out_file):
     if OPTION not in state.grid_options:
         state.grid_options.append(OPTION)
 
-    # A cache of core images.
-    cached_core_images = {}
-
     @state.change("selected_time", "selected_array", "selected_layer")
-    def update_core_view(selected_time, selected_array, selected_layer, **kwargs):
+    def update_core_view(selected_array, selected_layer, **kwargs):
         selected_layer = int(selected_layer)
-        image_data = None
 
-        # Extract from cache if possible
-        cache_key = (selected_layer, selected_array, selected_time)
-        if cache_key in cached_core_images:
-            # Shortcut if we have a cache. We might still need to redraw
-            # if the figure size was updated.
-            image_data = cached_core_images[cache_key]
+        array = getattr(vera_out_file.active_state, selected_array)
 
-        # Extract data from H5 + add to cache
-        if image_data is None:
-            array = getattr(vera_out_file.active_state, selected_array)
-            control_rod_positions = vera_out_file.core.control_rod_positions
+        # Load the layer and swap axes for faster indexing
+        layer_array = array[:, :, selected_layer].swapaxes(0, 2)
+        layer_array = layer_array.swapaxes(1, 2)
 
-            # Load the layer and swap axes for faster indexing
-            layer_array = array[:, :, selected_layer].swapaxes(0, 2)
-
-            reduced_core_map = vera_out_file.core.reduced_core_map
-            map_shape = reduced_core_map.shape
-
-            # Create the full sized image
-            image_shape = (map_shape[0] * array.shape[0], map_shape[1] * array.shape[1])
-            image_data = np.empty(image_shape, dtype=array.dtype)
-
-            # Populate it with the arrays
-            for i in range(reduced_core_map.shape[0]):
-                i_slice = slice(i * array.shape[0], (i + 1) * array.shape[0])
-                for j in range(reduced_core_map.shape[1]):
-                    j_slice = slice(j * array.shape[1], (j + 1) * array.shape[1])
-                    index = reduced_core_map[i, j]
-                    if index == 0:
-                        # Empty
-                        image_data[i_slice, j_slice] = np.nan
-                        continue
-
-                    assembly_array = layer_array[index - 1]
-
-                    # Set control rod positions to be nan
-                    assembly_array[control_rod_positions] = np.nan
-
-                    image_data[i_slice, j_slice] = assembly_array
-
-            # Only allow one image in the cache
-            MAX_ITEMS_IN_CACHE = 1
-            while len(cached_core_images) >= MAX_ITEMS_IN_CACHE:
-                cached_core_images.pop(next(iter(cached_core_images)))
-
-            cached_core_images[cache_key] = image_data
+        control_rod_positions = vera_out_file.core.control_rod_positions
+        reduced_core_map = vera_out_file.core.reduced_core_map
+        core_width = reduced_core_map.shape[0]
 
         # custom widget update
-        reduced_core_map = vera_out_file.core.reduced_core_map
-        assembly_width = array.shape[0]
-        core_width = map_shape[0]
         state.core_assemblies = []
-        for j in range(core_width):
+        for i in range(core_width):
             line = []
             state.core_assemblies.append(line)
-            for i in range(core_width):
-                if reduced_core_map[i, j]:
-                    assembly = image_data[
-                        slice(i * assembly_width, (i + 1) * assembly_width),
-                        slice(j * assembly_width, (j + 1) * assembly_width),
-                    ]
-                    line.append(np.ravel(assembly).tolist())
+            for j in range(core_width):
+                index = reduced_core_map[i, j] - 1
+                if index == -1:
+                    continue
+
+                assembly_array = layer_array[index]
+
+                # Set control rod positions to be nan
+                assembly_array[control_rod_positions] = np.nan
+
+                line.append(np.ravel(assembly_array).tolist())
 
     with DivLayout(server, template_name="core_view") as layout:
         layout.root.style = "height: 100%;"
