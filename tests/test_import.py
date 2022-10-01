612b86bd88769b25528e36e25a69ff0ade7afdb7
Patrick Avery <patrick.avery@kitware.com>
2022-10-01 14:13:25 -0400

    fix(tests): update import name in pytest
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,3 +1,3 @@
 def test_import():
     from vera_core import main  # noqa: F401
-    from vera_core.widgets.vera import TopQuadrant  # noqa: F401
+    from vera_core.widgets.vera import AxialView  # noqa: F401
